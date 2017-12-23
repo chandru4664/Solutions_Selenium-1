d4d66b78530fcfda3e923989d469172e37d40a64
sathishkumar0 <34356464+sathishkumar0@users.noreply.github.com>
2017-12-23 09:27:51 -0600

    Add files via upload

---
+++ NegativeSymbolinfactorial.java
@@ -0,0 +1,20 @@
+package domhtc.employee.eligibility;
+
+public class NegativeSymbolinfactorial {
+	public static void main(String[] args)
+    {
+int num [] =new int[]{1,3,5,7,9};
+String  n="-";
+
+for (int i=0;i< num.length;i++){
+if(i%2 != 0){
+System.out.println(n +num[i]);
+  }
+
+else {
+System.out.println(num[i]);
+    }
+}
+}
+
+}
