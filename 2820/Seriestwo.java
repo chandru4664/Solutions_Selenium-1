d17f62f7add2b96c3a88db75a839cdf269e423fe
spm1982 <34346664+spm1982@users.noreply.github.com>
2017-12-23 11:22:56 -0600

    Program for number series two

---
+++ 2820/Seriestwo.java
@@ -0,0 +1,12 @@
+package com.htc.exercise;
+
+public class Seriestwo {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.print("1" + "/" + i + ",");
+		}
+	}
+}
