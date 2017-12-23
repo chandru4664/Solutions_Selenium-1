f534c3d1c7e732bb948fc24181530051abbeac59
AjithRamesan <34376296+AjithRamesan@users.noreply.github.com>
2017-12-23 08:39:46 -0600

    Add files via upload

---
+++ 2589/SeriesLogic2.java
@@ -0,0 +1,24 @@
+package com.htc.javaexercise.exercise;
+
+public class SeriesLogic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+				for (int i = 1; i <= 15; i++) {    
+					if(i<15)
+					{
+						System.out.print("1/"+i+",");
+					}
+					if(i==15)
+					{
+						System.out.print("1/"+i);
+					}
+			
+				}
+				}
+			}
+
+
+	
