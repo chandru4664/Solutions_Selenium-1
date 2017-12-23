d021b458d8f1dcb9fed647fef20cad97c266a248
sathishkumar0 <34356464+sathishkumar0@users.noreply.github.com>
2017-12-23 10:36:58 -0600

    Factorial

---
+++ 2179/Factorial.java
@@ -0,0 +1,14 @@
+package domhtc.employee.eligibility;
+
+public class Factorial {
+	
+	 public static void main(String[] args)
+	    {
+	        int num [] = new int[]{1,2,3,4,5,6};
+	        
+	        for (int i=0;i<num.length;i++){
+	            System.out.println("1/"+num[i]);
+	        }
+	    }
+
+}
