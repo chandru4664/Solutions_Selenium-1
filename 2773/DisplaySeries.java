7792cb2fa1ca14adfbfa888d5e30cf8a12413ba8
Respaglory <34345777+Respaglory@users.noreply.github.com>
2017-12-26 18:00:11 -0500

    Add files via upload

---
+++ 2773/DisplaySeries.java
@@ -0,0 +1,18 @@
+package htc.com.Training.Excercises;
+
+//To Display nnumbers - 1/1,1/2,1/3...
+
+public class DisplaySeries {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		int n = 1;
+		int limit = 10;
+
+		for (i = 1; i <= limit; i++) {
+
+			System.out.println(n + "/" + i);
+		}
+	}
+}
